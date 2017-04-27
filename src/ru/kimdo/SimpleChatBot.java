0a9fc7e5b2729e40854cb285ae8ffc24b9dafe7a
palandr <palandrp@gmail.com>
2017-04-27 13:17:50 +0500

    Написано отображение пустого графического окошка

---
+++ src/ru/kimdo/SimpleChatBot.java
@@ -0,0 +1,33 @@
+package ru.kimdo;
+
+import javax.swing.*;
+
+/**
+ * Created by kimdo on 27.04.17.
+ */
+
+public class SimpleChatBot extends JFrame {
+
+    final String TITLE_OF_PROGRAM = "Chatter: simple chatbot";
+    final int START_LOCATION = 200;
+    final int WINDOW_WIDTH = 350;
+    final int WINDOW_HEIGHT = 450;
+
+    JTextArea dialogue;
+    JCheckBox ai;
+    JTextField message;
+//    SimpleBot sbot;
+
+    public static void main(String[] args) {
+        new SimpleChatBot();
+    }
+
+    SimpleChatBot() {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        setVisible(true);
+        
+    }
+}
