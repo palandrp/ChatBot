c7236d8dea87870527a4b7f5ee2c490b147a4193
palandr <palandrp@gmail.com>
2017-04-30 22:02:26 +0500

    Добавил имитацию простейшего диалога

--- src/ru/kimdo/SimpleBot.java
+++ src/ru/kimdo/SimpleBot.java
@@ -1,11 +1,51 @@
 package ru.kimdo;
 
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Created by kimdo on 27.04.17.
  */
-public class SimpleBot {
+class SimpleBot {
+    final String[] COMMON_PHRASES = {
+        "Нет ничего ценнее слов, сказанных к месту и ко времени.",
+        "Порой, молчание может сказать больше, нежели уйма слов.",
+        "Перед тем, как писать/говорить, всегда лучше подумать.",
+        "Вежливая и грамотная речь говорит о величии души.",
+        "Приятно, когда текст без орфографических ошибок.",
+        "Многословие - есть признак неупорядоченного ума.",
+        "Слова могут ранить, но могут и исцелять.",
+        "Записывая слова, мы удваиваем их силу.",
+        "Кто ясно мыслит, тот ясно излагает.",
+        "Боюсь, Вы что-то недоговариваете."
+    };
+    final String[] ELUSIV_ANSWERS = {
+        "Вопрос непростой, прошу тайм-аут на раздумья.",
+        "Не уверен, что располагаю такой информацией.",
+        "Может лучше поговорим о чем-то другом?",
+        "Простите, но это очень личный вопрос.",
+        "Не уверен, что вам понравится ответ.",
+        "Поверьте, я сам хотел бы это знать.",
+        "Вы действительно хотите это знать?",
+        "Уверен, вы уже догадались сами.",
+        "Зачем вам такая информация?",
+        "Давайте сохраним интригу?"
+    };
 
-    String sayInReturn(String message, boolean ai) {
-        return "Я отвечаю, значит я существую.";
+    Random random;
+    Date date;
+
+    SimpleBot(){
+        random = new Random();
+        date = new Date();
+        
+    }
+
+    String sayInReturn(String msg, boolean ai) {
+        String say = (msg.trim().endsWith("?"))?
+            ELUSIV_ANSWERS[random.nextInt(ELUSIV_ANSWERS.length)]:
+            COMMON_PHRASES[random.nextInt(COMMON_PHRASES.length)];
+
+        return say;
     }
 }
